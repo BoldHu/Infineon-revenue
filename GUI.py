724e48ed92267c185be01ee81f4c6174b890f3b8
Bold <dongchenghu@mail.ecust.edu.cn>
2023-11-22 17:51:22 +0800

    finish the GUI Part

--- GUI.py
+++ GUI.py
@@ -1,13 +1,113 @@
 import tkinter as tk
+from tkinter import filedialog
 
-class main(object):
+class GUI:
     def __init__(self):
+        # Create main window
         self.root = tk.Tk()
-        self.root.title("Test")
-        self.root.geometry("500x500")
-        self.root.resizable(0, 0)
+        self.root.title("Infineon Revenue Report Generator")
+        self.root.geometry("1000x800")
+        
+        # load the logo in images folder
+        self.logo = tk.PhotoImage(file="images/infineon.gif")
+        self.logo_label = tk.Label(self.root, image=self.logo)
+        self.logo_label.grid(row=0, column=0, columnspan=2)
+        
+        # Initialize file path variables
+        self.revord_path = tk.StringVar(value="")
+        self.ship_to_path = tk.StringVar(value="")
+        self.sold_to_path = tk.StringVar(value="")
+        self.allocation_path = tk.StringVar(value="")
+        self.dn_path = tk.StringVar(value="")
+        self.zm_path = tk.StringVar(value="")
 
-        self.label = tk.Label(self.root, text="Hello World")
-        self.label.pack()
-
+        # Maintain a list of file paths for easy access
+        self.file_paths = [self.revord_path, self.ship_to_path, self.sold_to_path, self.allocation_path, self.dn_path, self.zm_path]
+        
+    def run(self):
+        # Run the GUI
+        self.create_button_label()
         self.root.mainloop()
+
+    def upload_file(self, path, label):
+        # Upload a file and set the path
+        label.config(text="Uploading...")
+        path_selected = filedialog.askopenfilename()
+        path.set(path_selected)
+        label.set(path_selected)
+        
+    def select_save_path(self):
+        # Select the path to save the report
+        self.save_path.set("Selecting...")
+        path_selected = filedialog.askdirectory()
+        self.save_path.set(path_selected)
+        self.save_path_label.set(path_selected)
+        
+    def confirm_action(self):
+        # Placeholder for confirm action
+        print("Files to process:")
+        for path in self.file_paths:
+            print(path.get())
+
+    def clear_action(self):
+        # Clear all file paths
+        for path in self.file_paths:
+            path.set("")
+    
+    def create_button_label(self):
+        # Create labels and buttons for file uploads
+        # First is revord Excel
+        self.revord_path_label = tk.Label(self.root, textvariable=self.revord_path)
+        upload_revord = lambda: self.upload_file(self.revord_path, self.revord_path_label)
+        self.revord_upload_button = tk.Button(self.root, text="Upload Revord Excel", command=upload_revord)
+        self.revord_upload_button.grid(row=1, column=0, padx=(5, 10), pady=(40, 10))
+        self.revord_path_label.grid(row=1, column=1, padx=(5, 10), pady=(40, 10))
+        
+        # second is ship-to excel
+        self.ship_to_path_label = tk.Label(self.root, textvariable=self.ship_to_path)
+        upload_ship_to = lambda: self.upload_file(self.ship_to_path, self.ship_to_path_label)
+        self.ship_to_upload_button = tk.Button(self.root, text="Upload Ship-to Excel", command=upload_ship_to)
+        self.ship_to_upload_button.grid(row=2, column=0, padx=(5, 10), pady=(40, 10))
+        self.ship_to_path_label.grid(row=2, column=1, padx=(5, 10), pady=(40, 10))
+        
+        # third is sold-to excel
+        self.sold_to_path_label = tk.Label(self.root, textvariable=self.sold_to_path)
+        upload_sold_to = lambda: self.upload_file(self.sold_to_path, self.sold_to_path_label)
+        self.sold_to_upload_button = tk.Button(self.root, text="Upload Sold-to Excel", command=upload_sold_to)
+        self.sold_to_upload_button.grid(row=3, column=0, padx=(5, 10), pady=(40, 10))
+        self.sold_to_path_label.grid(row=3, column=1, padx=(5, 10), pady=(40, 10))
+        
+        # fourth is allocation excel
+        self.allocation_path_label = tk.Label(self.root, textvariable=self.allocation_path)
+        upload_allocation = lambda: self.upload_file(self.allocation_path, self.allocation_path_label)
+        self.allocation_upload_button = tk.Button(self.root, text="Upload Allocation Excel", command=upload_allocation)
+        self.allocation_upload_button.grid(row=4, column=0, padx=(5, 10), pady=(40, 10))
+        self.allocation_path_label.grid(row=4, column=1, padx=(5, 10), pady=(40, 10))
+        
+        # fifth is DN excel
+        self.dn_path_label = tk.Label(self.root, textvariable=self.dn_path)
+        upload_dn = lambda: self.upload_file(self.dn_path, self.dn_path_label)
+        self.dn_upload_button = tk.Button(self.root, text="Upload DN Excel", command=upload_dn)
+        self.dn_upload_button.grid(row=5, column=0, padx=(5, 10), pady=(40, 10))
+        self.dn_path_label.grid(row=5, column=1, padx=(5, 10), pady=(40, 10))
+
+        # sixth is ZM excel
+        self.zm_path_label = tk.Label(self.root, textvariable=self.zm_path)
+        upload_zm = lambda: self.upload_file(self.zm_path, self.zm_path_label)
+        self.zm_upload_button = tk.Button(self.root, text="Upload ZM Excel", command=upload_zm)
+        self.zm_upload_button.grid(row=6, column=0, padx=(5, 10), pady=(40, 10))
+        self.zm_path_label.grid(row=6, column=1, padx=(5, 10), pady=(40, 10))
+
+        # Create Confirm, save and Clear buttons
+        self.confirm_button = tk.Button(self.root, text="Confirm", command=self.confirm_action)
+        self.clear_button = tk.Button(self.root, text="Clear", command=self.clear_action)
+        self.save_button = tk.Button(self.root, text="Save", command=self.select_save_path)
+        self.save_path_label = tk.Label(self.root, textvariable=self.select_save_path)
+        self.save_button.grid(row=8, column=0, padx=(5, 10), pady=(40, 10))
+        self.save_path_label.grid(row=8, column=1, padx=(5, 10), pady=(40, 10))
+        self.confirm_button.grid(row=7, column=0, padx=(5, 10), pady=(40, 10))
+        self.clear_button.grid(row=7, column=1, padx=(5, 10), pady=(40, 10))
+
+# Create and run the GUI
+gui = GUI()
+gui.run()
